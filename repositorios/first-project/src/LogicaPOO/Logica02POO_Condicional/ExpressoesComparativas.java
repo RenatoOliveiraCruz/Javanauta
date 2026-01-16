3cd9e60eec287f986bfb478706577802a24e1854
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-16 16:50:11 -0300

    Add LogicaPOO2_Condicional - Expressões_Comparativas

---
+++ repositorios/first-project/src/LogicaPOO/Logica02POO_Condicional/ExpressoesComparativas.java
@@ -0,0 +1,22 @@
+package LogicaPOO.Logica02POO_Condicional;
+
+import java.util.Scanner;
+
+public class ExpressoesComparativas {
+    public ExpressoesComparativas() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantos anos você tem?");
+        double voce = scanner.nextDouble();
+        System.out.println("E o parça?");
+        double para = scanner.nextDouble();
+        if (voce < para) {
+            System.out.println("Você é mais novo");
+        } else {
+            System.out.println("Você é mais velho");
+        }
+
+    }
+}
