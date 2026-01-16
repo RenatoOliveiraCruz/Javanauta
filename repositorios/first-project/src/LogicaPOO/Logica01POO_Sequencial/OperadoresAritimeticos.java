e362272dc89b9a01190dda9c409a902d906b3b52
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-16 09:02:43 -0300

    Operadores Aritiméticos

---
+++ repositorios/first-project/src/LogicaPOO/Logica01POO_Sequencial/OperadoresAritimeticos.java
@@ -0,0 +1,16 @@
+package LogicaPOO.Logica01POO_Sequencial;
+
+public class OperadoresAritimeticos {
+
+    public static void main(String[] args) {
+        int ingredientes = 5;
+        double preco = 150.49;
+        char role = 'J';
+        boolean sl = false;
+
+        System.out.println(ingredientes);
+        System.out.println(preco);
+        System.out.println(role);
+        System.out.println(sl);
+    }
+}
