325a6ed058407ac8e59eeefb47fe181f70ad30f6
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-16 07:57:11 -0300

    Start to lean Java

---
+++ repositorios/first-project/src/LogicaPOO/Logica01POO_Sequencial/Main.java
@@ -0,0 +1,15 @@
+package LogicaPOO.Logica01POO_Sequencial;
+
+public class Main {
+    public static void main(String[] args) {
+        int idade = 25;
+        double salarialMensal = 4500.00;
+        boolean empregagoJava = true;
+        char genero = 'M';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário mensal: " + salarialMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Empregado: " + empregagoJava);
+    }
+}
