a1be4f685aacbd88299814dfaab5682651f28804
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-19 08:40:01 -0300

    Add IfElse

---
+++ repositorios/first-project/src/LogicaPOO/Logica02POO_Condicional/EstruturaIfElse.java
@@ -0,0 +1,26 @@
+package LogicaPOO.Logica02POO_Condicional;
+
+public class EstruturaIfElse {
+    public EstruturaIfElse() {
+    }
+
+    public static void main(String[] args) {
+        int age = 22;
+        if (age >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+
+        boolean ps5 = true;
+        boolean pc = true;
+        if (pc && ps5) {
+            System.out.println("Seu setup é foda!!!");
+        } else if (!pc && !ps5) {
+            System.out.println("Vish, que pena, você ta fora da jogatina");
+        } else {
+            System.out.println("Que maneiro ter uma plataforma pra jogar né?!");
+        }
+
+    }
+}
