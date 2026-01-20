b91df16a274166aaefc43a24814cff9754bace83
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-20 14:58:10 -0300

    Add Remember(if&scanner)

---
+++ repositorios/first-project/src/LogicaPOO/POO/Objetos/Remember.java
@@ -0,0 +1,18 @@
+package LogicaPOO.POO.Objetos;
+import java.util.Scanner;
+
+public class Remember {
+    public Remember() {
+    }
+    public static void main(String[] args) {
+        System.out.println("Olhos vermelhos e que soltam lasers. Quem sou eu?");
+        Scanner scott = new Scanner(System.in);
+        String xmen = scott.nextLine();
+        if (!xmen.equals("Scott") && !xmen.equals("Ciclope")) {
+            System.out.println("Errou ");
+        } else {
+            System.out.println("Ai sim carai, sou eu mesmo!!! ");
+        }
+
+    }
+}
