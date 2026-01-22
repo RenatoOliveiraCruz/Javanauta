07944add08d6ac7ac9c4bdc5d633bdcf9c75634b
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-22 16:45:27 -0300

    Add Exercises.POO.EX01_Métodos2 *Livros

---
+++ repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Metodos2/Livro.java
@@ -0,0 +1,11 @@
+package LogicaPOO.Exercicios.POO.EX01_Metodos2;
+
+public class Livro {
+        String nome;
+        String autor;
+        String editora;
+        int qtdPaginas;
+        String categoria;
+        String Genero;
+        String idiomaOriginal;
+}
---
+++ repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Metodos2/Main.java
@@ -0,0 +1,25 @@
+package LogicaPOO.Exercicios.POO.EX01_Metodos2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Livro livro1 = new Livro();
+        livro1.nome = "Preço da Desonta";
+        livro1.autor = "Hiroshi Hirata";
+        livro1.qtdPaginas = 385 ;
+        livro1.categoria = "História em Quadrinho/Mangá";
+        livro1.editora = "Pipoca e Nanquin";
+        livro1.Genero = "Drama";
+        livro1.idiomaOriginal = "Japonês";
+
+        System.out.println("NOME - " + livro1.nome);
+        System.out.println("AUTOR - " + livro1.autor);
+        System.out.println("QUANTIDADE DE PÁGINAS - " + livro1.qtdPaginas);
+        System.out.println("CATEGORIA - " + livro1.categoria);
+        System.out.println("EDITORA - " + livro1.editora);
+        System.out.println("GÊNERO - " + livro1.Genero);
+        System.out.println("IDIOMA ORIGINAL - " + livro1.idiomaOriginal);
+    }
+}
