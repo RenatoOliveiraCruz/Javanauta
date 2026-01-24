115c8c260e65a8738ba972f430c0bae818bde41a
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-24 09:54:04 -0300

    feat Atleta.java

---
+++ repositorios/first-project/src/LogicaPOO/POO/Atleta/Atleta.java
@@ -0,0 +1,23 @@
+package LogicaPOO.POO.Atleta;
+
+public class Atleta {
+    String etnia;
+    String modalidade;
+    double altura;
+    double peso;
+
+    public Atleta(String etnia, String modalidade, double altura, double peso) {
+        this.etnia = etnia;
+        this.modalidade = modalidade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public void correr() {
+        System.out.printf("Correndo eficientemente");
+    }
+
+    public void pula() {
+        System.out.printf("Pulando alto");
+    }
+}
