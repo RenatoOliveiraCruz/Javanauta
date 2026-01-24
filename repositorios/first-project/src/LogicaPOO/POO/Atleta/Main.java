775471f43dbc63bd28a1833e2a0ebbb991d1c948
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-24 11:30:37 -0300

    feat: Main.java

---
+++ repositorios/first-project/src/LogicaPOO/POO/Atleta/Main.java
@@ -0,0 +1,13 @@
+package LogicaPOO.POO.Atleta;
+
+public class Main {
+    public static void main (String[] args) {
+        //Criando o Objeto (Instanciando)
+        Duble myDuble = new Duble("Pardo","MMA",1.75,85.5, true);
+
+        System.out.println("Atleta de " + myDuble.modalidade);
+
+        myDuble.correr();           // Método herdado do Atleta
+        myDuble.quedaPericulosa();  // Método exclusivo do Dublê
+    }
+}
