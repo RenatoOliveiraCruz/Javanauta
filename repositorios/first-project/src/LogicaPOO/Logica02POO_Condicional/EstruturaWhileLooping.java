80e55b11d049b386b81d672c8a251edac05b8cd9
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-29 08:12:35 -0300

    feat: Estrutura While

---
+++ repositorios/first-project/src/LogicaPOO/Logica02POO_Condicional/EstruturaWhileLooping.java
@@ -0,0 +1,12 @@
+package LogicaPOO.Logica02POO_Condicional;
+
+public class EstruturaWhileLooping {
+    public static void main(String[] args) {
+        int volta = 0;
+
+        while (volta <= 10) {
+            System.out.println("Volta número " + volta + " concluída!");
+            volta++;
+        }
+    }
+}
