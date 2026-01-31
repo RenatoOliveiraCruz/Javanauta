7d09df8c16b19780d67825ef00de30eb8a84fa34
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-01-31 09:48:03 -0300

    feat: get & set

--- repositorios/first-project/src/LogicaPOO/POO/Atleta/Atleta.java
+++ repositorios/first-project/src/LogicaPOO/POO/Atleta/Atleta.java
@@ -3,8 +3,18 @@ package LogicaPOO.POO.Atleta;
 public class Atleta {
     String etnia;
     String modalidade;
-    double altura;
+   private double altura;
     double peso;
+
+    // Set => O Segurança
+    public void setAltura(double novaAltura){
+        if(novaAltura < 1.0 || novaAltura > 2.3){
+            System.out.println("ERRO: Altura inválida para um atleta!");
+        } else {
+            this.altura = novaAltura;  // Só aceita se estiver nos parâmetros
+            System.out.println("Altura atualizada com sucesso.");
+        }
+    }
 
     // Construtor (O manual de como montar o atleta)
     public Atleta(String etnia, String modalidade, double altura, double peso) {
@@ -16,10 +26,16 @@ public class Atleta {
 
     // Métodos (Ações)
     public void correr() {
-        System.out.printf("Correndo eficientemente\n");
+        System.out.println("Correndo eficientemente\n");
     }
 
     public void pula() {
-        System.out.printf("Pulando alto");
+        System.out.println("Pulando alto");
+    }
+
+
+    // Get => O Informante
+    public double getAltura(){
+        return this.altura;
     }
 }
