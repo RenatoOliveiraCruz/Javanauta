47a79d19de146e870c07efa9d6f48c4ac9a912c0
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-02-23 16:20:37 -0300

    feat: funct- mostrar informações

--- repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Metodos3/Aluno.java
+++ repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Metodos3/Aluno.java
@@ -7,4 +7,21 @@ public class Aluno {
     String[] disciplinas = new String[3];
     double[][] notasDisciplinas = new double[3][4]; // 3 para as Disciplinas e a média
 
+    void mostrarInformacoes(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Nome do curso: " + curso);
+
+        for (int i = 0; i < notasDisciplinas.length; i++){
+            System.out.println("Notas da disciplina " + disciplinas[i]);
+            for (int j = 0; j < notasDisciplinas[i].length; j++){
+                System.out.println(notasDisciplinas[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
 }
