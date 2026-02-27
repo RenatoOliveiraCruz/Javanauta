2565839b59b758ad36c3487b9be9649e0cccb85a
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-02-27 11:37:37 -0300

    feat: Heranca.Animal

---
+++ repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Heranca/Animal.java
@@ -0,0 +1,14 @@
+package LogicaPOO.Exercicios.POO.EX01_Heranca;
+
+public class Animal {
+        private String nome;
+        private int comprimento;
+        private int patas;
+        private String cor;
+        private String ambiente;
+        private double velocidade;
+
+        public Animal() {
+            this.patas = 4;
+        }
+}
