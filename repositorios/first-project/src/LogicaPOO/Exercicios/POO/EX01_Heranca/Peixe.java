057e25829212721841321164c1e8d4088c9308b3
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-03-02 11:32:35 -0300

    feat: Heranca.Animal--ext--Peixe

---
+++ repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Heranca/Peixe.java
@@ -0,0 +1,14 @@
+package LogicaPOO.Exercicios.POO.EX01_Heranca;
+
+public class Peixe extends Animal{
+    private String caracteristicas;
+
+    public Peixe() {
+        super();
+        this.setPatas(0);
+        this.setAmbiente("Mar");
+        this.setCor("Cinzenta");
+        this.caracteristicas = "Barbatanas e cauda";
+    }
+
+}
