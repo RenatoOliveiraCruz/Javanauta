3165cc820e7159d8f779c53192ca75071679e6f1
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-03-02 11:39:44 -0300

    feat: Heranca03.Main

---
+++ repositorios/first-project/src/LogicaPOO/Exercicios/POO/EX01_Heranca/Main.java
@@ -0,0 +1,15 @@
+package LogicaPOO.Exercicios.POO.EX01_Heranca;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Animal camelo = new Animal();
+        camelo.setNome("Camelo");
+        camelo.setComprimento(150);
+
+        camelo.setCor("Amarelo");
+        camelo.setAmbiente("Terra");
+        camelo.setVelocidade(2);
+
+    }
+}
