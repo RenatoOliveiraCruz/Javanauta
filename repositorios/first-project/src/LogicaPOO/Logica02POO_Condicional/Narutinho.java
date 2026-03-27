0f33fa103c0ca55bef00f601d33b47ed0fae669f
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-03-27 16:55:28 -0300

    feat: narutinho

---
+++ repositorios/first-project/src/LogicaPOO/Logica02POO_Condicional/Narutinho.java
@@ -0,0 +1,12 @@
+package LogicaPOO.Logica02POO_Condicional;
+
+public class Narutinho {
+    public static void main(String[] args) {
+
+        String nome = "Kakashi Hatake";
+        int idade = 30;
+        boolean hokage = true;
+        short numerodeMissoes = 4213;
+
+    }
+}
