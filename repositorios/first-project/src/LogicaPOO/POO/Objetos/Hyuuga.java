28fdc3bc9cc1de826b17376fdc656dfbe33496c8
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-03-27 11:19:13 -0300

    feat: Hyuuga's

--- repositorios/first-project/src/LogicaPOO/POO/Objetos/Hyuuga.java
+++ repositorios/first-project/src/LogicaPOO/POO/Objetos/Hyuuga.java
@@ -1,9 +1,34 @@
 package LogicaPOO.POO.Objetos;
 
 public class Hyuuga {
-    String nomeHyuuga1 = "Nejiy";
-    String nomeHyuuga2 = "Hinata";
-    String nomeHhyuuga3 = "Hanabi";
+    public static void main(String[] args) {
 
 
+    String nameHyuuga1 = "Nejiy";
+    int ageHyuuga1 = 20;
+    String missionHyuuga1 = "Protect the princess Hinata.";
+    String missionStatus1 = "Loading...";
+    char missionLevelHyuuga1 = 'B';
+        System.out.println("Name of the 1º Hyuuga: " + nameHyuuga1);
+        System.out.println("Age of the 1º Hyuuga: " + ageHyuuga1);
+        System.out.println("The mission of the 1º Hyuuga: " + missionHyuuga1);
+
+    String nameHyuuga2 = "Hinata";
+    int ageHyuuga2 = 19;
+    String missionHyuuga2 = "Find the enemys.";
+    String missionStatus2 = "Done";
+    char missionLevelHyuuga2 = 'B';
+        System.out.println("Name of the 2º Hyuuga: " + nameHyuuga2);
+        System.out.println("Age of the 2º Hyuuga: " + ageHyuuga2);
+        System.out.println("The mission of the 2º Hyuuga: " + missionHyuuga2);
+
+    String nameHyuuga3 = "Hanabi";
+    int ageHyuuga3 = 14;
+    String missionHyuuga3 = "Beat the enemys.";
+    String missionStatus3 = "Loading...";
+    char missionLevelHyuuga3 = 'S';
+        System.out.println("Name of the 3º Hyuuga: " + nameHyuuga3);
+        System.out.println("Age of the 3º Hyuuga: " + ageHyuuga3);
+        System.out.println("The mission of the 3º Hyuuga: " + missionHyuuga3);
+    }
 }
